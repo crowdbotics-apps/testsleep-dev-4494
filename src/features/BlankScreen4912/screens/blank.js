699074208f9617b2f1b6569618bdcf53ca50748d
Crowdbotics <team@crowdbotics.com>
2020-05-31 12:58:47 +0000

    Update blank.js JSX

--- src/features/BlankScreen4912/screens/blank.js
+++ src/features/BlankScreen4912/screens/blank.js
@@ -1,19 +1,122 @@
-import React from "react";
-import { View } from "react-native";
-import { withStyles, Text, Button, CheckBox, Radio, Toggle, Icon, Input } from 'react-native-ui-kitten';
+import React from "react"
+import { View } from "react-native"
+import {
+  withStyles,
+  Text,
+  Button,
+  CheckBox,
+  Radio,
+  Toggle,
+  Icon,
+  Input
+} from "react-native-ui-kitten"
 
 export class _Blank extends React.Component {
   static navigationOptions = {
     title: "Blank".toUpperCase()
-  };
+  }
 
-  state = { }
+  state = {}
 
-  render = () => <View style={this.props.themedStyle.container}></View>;
+  render = () => (
+    <View
+      style={{
+        overflow: "visible",
+        flexDirection: "column",
+        flexWrap: "wrap",
+        justifyContent: "flex-start",
+        alignItems: "stretch",
+        alignContent: "stretch",
+        borderStyle: "solid",
+        backgroundSize: "auto"
+      }}
+    >
+      <View
+        style={{
+          overflow: "visible",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "flex-start",
+          alignItems: "stretch",
+          alignContent: "stretch",
+          borderStyle: "solid",
+          backgroundSize: "auto"
+        }}
+      >
+        <View
+          style={{
+            overflow: "visible",
+            flexDirection: "column",
+            flexWrap: "wrap",
+            justifyContent: "flex-start",
+            alignItems: "stretch",
+            alignContent: "stretch",
+            borderStyle: "solid",
+            backgroundSize: "auto"
+          }}
+        />
+        <View
+          style={{
+            overflow: "visible",
+            flexDirection: "column",
+            flexWrap: "wrap",
+            justifyContent: "flex-start",
+            alignItems: "stretch",
+            alignContent: "stretch",
+            borderStyle: "solid",
+            backgroundSize: "auto"
+          }}
+        />
+      </View>
+      <View
+        style={{
+          overflow: "visible",
+          flexDirection: "column",
+          flexWrap: "wrap",
+          justifyContent: "flex-start",
+          alignItems: "stretch",
+          alignContent: "stretch",
+          borderStyle: "solid",
+          backgroundSize: "auto"
+        }}
+      >
+        <View
+          style={{
+            overflow: "visible",
+            flexDirection: "column",
+            flexWrap: "wrap",
+            justifyContent: "flex-start",
+            alignItems: "stretch",
+            alignContent: "stretch",
+            borderStyle: "solid",
+            backgroundSize: "auto"
+          }}
+        >
+          <Image
+            resizeMethod="cover"
+            style={{ overflow: "visible", borderStyle: "solid" }}
+          />
+        </View>
+        <View
+          style={{
+            overflow: "visible",
+            flexDirection: "column",
+            flexWrap: "wrap",
+            justifyContent: "flex-start",
+            alignItems: "stretch",
+            alignContent: "stretch",
+            borderStyle: "solid",
+            backgroundSize: "auto"
+          }}
+        />
+      </View>
+      <Text style={{ overflow: "visible", borderStyle: "solid" }}>Text</Text>
+    </View>
+  )
 }
 
 export default Blank = withStyles(_Blank, theme => ({
   container: {
     backgroundColor: theme["color-basic-100"]
   }
-}));
+}))
